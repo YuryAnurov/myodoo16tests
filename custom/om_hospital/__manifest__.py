8a90e5600bbb6c348188e2592ee990af47579620
Yura <yuryanurov@gmail.com>
2024-01-31 20:30:12 +0400

    step1 -  model, menu and views created

---
+++ custom/om_hospital/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Hospital Management System',
+    'author': 'Yura',
+    'summary': 'Odoo 16 development',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}
